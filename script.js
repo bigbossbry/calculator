0a3d2143917fda8c47aa0855085670f2859a3c1a
bigbossbry <brycmramirez@gmail.com>
2023-01-19 19:15:15 +0800

    Finish second iteration of calculator layout.

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const btnsContainer = document.querySelector('.btns-container');
+const arr1 = ['+', '-', '*', '/'];
+const arr2 = ['0', '.', '='];
+const arr3 = ['AC', 'C', '+/-', '%'];
 
 for (let i = 0; i<20; i++) {
-    const buttons = document.createElement('div');
-    buttons.classList.add('calc-button');
-    buttons.classList.add(`btn-${i+1}`);
-    btnsContainer.appendChild(buttons);
-    
+    const calcBtn = document.createElement('button');
+    calcBtn.classList.add('calc-button');
+    calcBtn.classList.add(`btn-${i}`);
+    btnsContainer.appendChild(calcBtn);
+    if (i>=0 && i <=3) {calcBtn.textContent = `${arr3[i]}`;}
+    if (i>=4 && i <=6) {calcBtn.textContent = `${i+3}`;}
+    if (i>=8 && i <=10) {calcBtn.textContent = `${i-4}`;}
+    if (i>=12 && i <=14) {calcBtn.textContent = `${i-11}`;}
+    if (i>=16 && i <=18) {calcBtn.textContent = `${arr2[i-16]}`;}
+    if (i==7) {calcBtn.textContent = `${arr1[3]}`;}
+    if (i==11) {calcBtn.textContent = `${arr1[2]}`;}
+    if (i==15) {calcBtn.textContent = `${arr1[1]}`;}
+    if (i==19) {calcBtn.textContent = `${arr1[0]}`;}
 }
+
+
+
+
+
